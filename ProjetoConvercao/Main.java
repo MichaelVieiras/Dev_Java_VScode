8bd0017f9c3f40e50eb80d35a4d6ad690f8d881f
Michael Vieira <devmchvds@gmail.com>
2022-09-14 18:12:34 -0300

    Update Projeto Convercao

--- ProjetoConvercao/Main.java
+++ ProjetoConvercao/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Conversor conversor = new Conversor();
 
-        
         System.out.println("Digite a quantidade de Galões");
         conversor.galoes = scan.nextDouble();
+
         conversor.info();
 
         scan.close();
@@ -15,12 +15,8 @@ public class Main {
 }
 
 /*
- * Crie um projeto que converta galões em litros.O programa
- * funcionará declarando duas variáveis double. Uma conterá o número de
- * galões e a outra o número de litros após a conversão.
- * Um galão é equivalente a 3,7854 litros. Logo, na conversão de galões em
- * litros,
- * o valor do galão é multiplicado por 3,7854.
- * O programa exibe tanto o número de galões quanto o número equivalente em
- * litros.
+ * Crie um projeto que converta galões em litros.O programa funcionará declarando  
+ * duas variáveis double. Uma conterá o número de galões e a outra o número de litros * após a conversão.
+ * Um galão é equivalente a 3,7854 litros. Logo, na conversão de galões em litros, o  * valor do galão é multiplicado por 3,7854.
+ * O programa exibe tanto o número de galões quanto o número equivalente em litros.
  */
